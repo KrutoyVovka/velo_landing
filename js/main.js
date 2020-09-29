a6829cf1191ed1ac4fe2e91b606cd1233ae5eb8d
Vladimir <zdescodim@gmail.com>
2020-09-30 00:45:56 +0600

    "header" - adaptiv

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('.wrapper').addClass('loaded');
+
+$('icon-menu').click(function (event) {
+	$(this).toggleClass('active');
+	$('.meny__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+
+function ibg(){ // -- jquery --
+	$.each($('.ibg'), function(index, val) {
+		if($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
+/* --чистый JS--
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg(); */
